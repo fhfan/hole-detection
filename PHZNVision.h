40ed909c404aacbf476e21f9f8e64d9c032a750e
fhfan <fhuafan@126.com>
2021-02-16 18:21:23 +0800

    PHZNVision.h

---
+++ PHZNVision.h
@@ -0,0 +1,32 @@
+
+// PHZNVision.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPHZNVisionApp: 
+// 有关此类的实现，请参阅 PHZNVision.cpp
+//
+
+class CPHZNVisionApp : public CWinApp
+{
+public:
+	CPHZNVisionApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPHZNVisionApp theApp;
